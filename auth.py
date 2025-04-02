85c667e5e1913f0bb53d5d2938b0a1c5579f3bcd
Saksham Sodhi <sodhisaksham18@gmail.com>
2025-04-02 14:23:44 +0530

    auth.py

---
+++ auth.py
@@ -0,0 +1,7 @@
+import bcrypt
+
+def hash_password(password):
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+def verify_password(stored_password, provided_password):
+    return bcrypt.checkpw(provided_password.encode(), stored_password.encode())
